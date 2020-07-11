f9e00947cc73824a4e52b86b835941741a4ba887
Ana Carolina Scardovelli <anacarolinascardovelli@gmail.com>
2020-07-10 21:20:34 -0300

    kk

---
+++ lib/main.dart
@@ -0,0 +1,42 @@
+import 'package:dynamic_theme/dynamic_theme.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:interest_calculator/pages/calculator_page.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+    ]);
+
+    return DynamicTheme(
+      defaultBrightness: Brightness.light,
+      data: (brightness) => new ThemeData(
+        primarySwatch: Colors.amber,
+        brightness: brightness,
+      ),
+      themedWidgetBuilder: (context, theme) {
+        return MaterialApp(
+      title: 'Calculadora de Juros',
+      theme: theme,
+      debugShowCheckedModeBanner: false,
+      home: CalculatorPage(),
+      localizationsDelegates: [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: [
+        const Locale('pt', 'BR'),
+        const Locale('en', 'US'),
+      ],
+    );
+      }
+    );
+  }
+ 
+  }
